07fa152e4f269fff546356e104bcc5e86148cb6a
Cuh4 <roozzguy@gmail.com>
2023-10-24 07:43:39 +0100

    rename warning

--- src/example_config.py
+++ src/example_config.py
@@ -4,3 +4,5 @@
 
 botToken = "" # token of your discord bot. create a bot here: https://discord.com/developers/applications'
 chatCooldown = 4 # cooldown given to each user. if a user tries to talk to the bot twice in {chatCooldown} seconds, the query will not be processed
+
+# [!!] Rename to config.py [!!]
